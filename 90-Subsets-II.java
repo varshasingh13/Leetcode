2722a9efc5d87c069fdea1b5d0574f8d5d200e72
varshasingh13 <142929062+varshasingh13@users.noreply.github.com>
2024-09-05 11:16:44 -0400

    [90] [Time Beats: 99.89%] [Memory Beats: 67.35%] - LeetPush

---
+++ 90-Subsets-II.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> temp = new ArrayList<>();
+         Arrays.sort(nums); 
+        backTrack(ans, 0, nums, temp);
+        return ans;
+    }
+
+    private void backTrack(List<List<Integer>> ans, int start, int[] nums, List<Integer> temp){
+        ans.add(new ArrayList<>(temp));
+        for(int i=start; i<nums.length; i++){
+            if(i> start && nums[i] == nums[i-1]){
+                continue;
+            }
+            temp.add(nums[i]);
+            backTrack(ans, i+1, nums, temp);
+            temp.remove(temp.size() - 1);
+        }
+    }
+}
