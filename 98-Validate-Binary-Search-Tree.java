54f98ce080be336c8e5ec3250b7481f450708a9e
varshasingh13 <142929062+varshasingh13@users.noreply.github.com>
2024-09-14 21:09:11 -0400

    [98] [Time Beats: 14.90%] [Memory Beats: 27.80%] - LeetPush

--- 98-Validate-Binary-Search-Tree.java
+++ 98-Validate-Binary-Search-Tree.java
@@ -16,17 +16,36 @@
 class Solution {
     public boolean isValidBST(TreeNode root) {
         
-        return isValidBST(root,Long.MIN_VALUE,Long.MAX_VALUE);
+        //return isValidBST(root,Long.MIN_VALUE,Long.MAX_VALUE);
+        ArrayList<Integer> l = new ArrayList<>();
+        
+        inOrder(root, l);
+        for(int i=0; i<l.size()-1;i++){
+            if(l.get(i)>=l.get(i+1)){
+                return false;
+
+            }
+        }
+        return true;
         
     }
 
-    public boolean isValidBST(TreeNode root,long min,long max){
-        if(root == null)
-        return true;
-
-        if(root.val>=max || root.val<=min)
-        return false;
-
-        return isValidBST(root.left, min, root.val) && isValidBST(root.right, root.val, max);
+   private void inOrder( TreeNode root, ArrayList<Integer> l){
+    if(root != null){
+   
+    inOrder(root.left, l);
+    l.add(root.val);
+    inOrder(root.right, l);
     }
+   }
+
+    // public boolean isValidBST(TreeNode root,long min,long max){
+    //     if(root == null)
+    //     return true;
+
+    //     if(root.val>=max || root.val<=min)
+    //     return false;
+
+    //     return isValidBST(root.left, min, root.val) && isValidBST(root.right, root.val, max);
+    // }
 }
