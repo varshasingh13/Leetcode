2f500d30fe572418315ad5c592529ab8db275dc6
varshasingh13 <142929062+varshasingh13@users.noreply.github.com>
2024-09-22 00:31:17 -0400

    [152] [Time Beats: 94.00%] [Memory Beats: 44.27%] - LeetPush

--- 152-Maximum-Product-Subarray.java
+++ 152-Maximum-Product-Subarray.java
@@ -2,24 +2,42 @@ class Solution {
     public int maxProduct(int[] nums) {
         if (nums.length == 0) return 0;
 
-        int maxProduct = nums[0];
-        int minProduct = nums[0];
-        int result = nums[0];
+        // int maxProduct = nums[0];
+        // int minProduct = nums[0];
+        // int result = nums[0];
+        int pre =1;
+        int suff =1;
+        int result = Integer.MIN_VALUE;
 
-        for (int i = 1; i < nums.length; i++) {
-            if (nums[i] < 0) {
-                // Swap maxProduct and minProduct when nums[i] is negative
-                int temp = maxProduct;
-                maxProduct = minProduct;
-                minProduct = temp;
-            }
+        for (int i = 0; i < nums.length; i++) {
 
-            // Update maxProduct and minProduct
-            maxProduct = Math.max(nums[i], maxProduct * nums[i]);
-            minProduct = Math.min(nums[i], minProduct * nums[i]);
+            //Approach 1
+            if(pre == 0)
+            pre =1;
 
-            // Update the result
-            result = Math.max(result, maxProduct);
+            if(suff == 0)
+            suff =1;
+
+            pre = pre *nums[i];
+            suff = suff*nums[nums.length-i-1];
+            result = Math.max(result, Math.max(pre,suff));
+
+
+            //Approach -2
+            // if (nums[i] < 0) {
+            //     // Swap maxProduct and minProduct when nums[i] is negative
+            //     int temp = maxProduct;
+            //     maxProduct = minProduct;
+            //     minProduct = temp;
+            // }
+
+            // // Update maxProduct and minProduct
+            // maxProduct = Math.max(nums[i], maxProduct * nums[i]);
+            // minProduct = Math.min(nums[i], minProduct * nums[i]);
+
+            // // Update the result
+            // result = Math.max(result, maxProduct);
+
         }
 
         return result;
