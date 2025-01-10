bbe8d32219a7f5c2eb983e5a3d18eb33b28e4f0d
varshasingh13 <142929062+varshasingh13@users.noreply.github.com>
2025-01-09 22:01:37 -0500

    [217] [Time Beats: 92.01%] [Memory Beats: 5.51%] - LeetPush

--- 217-Contains-Duplicate.java
+++ 217-Contains-Duplicate.java
@@ -1,12 +1,22 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        Arrays.sort(nums);
-        for(int i=0;i<nums.length-1;i++){
-            if(nums[i] == nums[i+1]){
-                return true;
+        //  Arrays.sort(nums);
+        // for(int i=0;i<nums.length-1;i++){
+        //     if(nums[i] == nums[i+1]){
+        //         return true;
 
+        //     }
+        // }
+        // return false;
+
+        Set<Integer> s= new HashSet<>();
+        for(int n: nums){
+            if(!s.add(n)){
+                return true;
             }
+           // s.add(n);
+           
         }
-        return false;
+         return false;
     }
 }
