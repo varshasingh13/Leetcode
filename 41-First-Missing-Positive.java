7f738598b39e969e0c17197fc839718b79707217
varshasingh13 <142929062+varshasingh13@users.noreply.github.com>
2025-01-28 01:28:53 -0500

    [41] [Time Beats: 32.65%] [Memory Beats: 10.87%] - LeetPush

--- 41-First-Missing-Positive.java
+++ 41-First-Missing-Positive.java
@@ -1,10 +1,11 @@
 class Solution {
     public int firstMissingPositive(int[] nums) {
-        Arrays.sort(nums);
-         //List<Integer> l = new ArrayList<>();
+        // Arrays.sort(nums); 
+        // List<Integer> l = new ArrayList<>();
          HashSet<Integer> s = new HashSet<>();
         for (int num : nums) {
             s.add(num);
+            
         }
         int missingValue =1;
         while(missingValue<=nums.length){
